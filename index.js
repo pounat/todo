1a8e033a125e009b4110d84306edad5442d3a796
Pounat <nathan7418@gmail.com>
2020-03-22 01:38:47 -0600

    wow

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const bodyParser = require(`body-parser`);
+const mongoose = require(`mongoose`);
+const routes = require(`./routes/api`);
+const path = require(`path`);
 require('dotenv').config();
 
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+//connect to the database
+mongoose.connect(process.env.DB, ( useNewUrlParser: true ))
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
